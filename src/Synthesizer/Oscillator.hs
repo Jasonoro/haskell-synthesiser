a853f76497c6f22d2dbd9b2916c8e8f11bbf7a87
Vince van Noort <vincevannoort@gmail.com>
2021-03-11 15:33:49 +0100

    Added Sine and SawTooth oscillators

---
+++ src/Synthesizer/Oscillator.hs
@@ -0,0 +1,27 @@
+module Oscillator where
+
+import Data.Fixed
+import Numeric
+import Prelude               hiding (cycle)
+import Synthesizer.Structure
+
+type WaveFunction = Double -> Double
+
+sawToothOscillator :: Frequency -> SamplingRate -> [Double]
+sawToothOscillator frequency = oscillator sawTooth frequency
+  where
+    sawTooth :: WaveFunction
+    sawTooth x = x `mod'` frequency
+
+sineOscillator :: Frequency -> SamplingRate -> [Double]
+sineOscillator = oscillator sin
+
+oscillator :: WaveFunction -> Frequency -> SamplingRate -> [Double]
+oscillator waveFunction frequency samplingRate = waveFunction <$> [0, delta..]
+  where
+    sr :: Double
+    sr = fromIntegral samplingRate
+    cycle :: Double
+    cycle = 2 * pi
+    delta :: Double
+    delta = (frequency * cycle) / sr
