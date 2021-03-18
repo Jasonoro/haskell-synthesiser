05c6e3cb45f5843b58c19042d7fadf767db02c3b
Vince van Noort <vincevannoort@gmail.com>
2021-03-18 14:10:41 +0100

    Removed old comments

--- app/Main.hs
+++ app/Main.hs
@@ -14,23 +14,6 @@ volume = 32767
 mediumPrioritySignal :: SynSound
 mediumPrioritySignal = SynSound [
         Channel (playChord 0.0 2.0 (volume / 5) ('C', 3))
-        -- ++
-        -- (playChord 1.0 2.0 (volume / 5) ('C', 2))
-        -- [
-        --     playNote 0.0 0.5 volume ('C', 3),
-        --     playNote 0.5 0.5 volume ('D', 3),
-        --     playNote 1.0 0.5 volume ('E', 3),
-        --     playNote 1.5 0.5 volume ('C', 3),
-
-        --     playNote 2.0 0.5 volume ('C', 3),
-        --     playNote 2.5 0.5 volume ('D', 3),
-        --     playNote 3.0 0.5 volume ('E', 3),
-        --     playNote 3.5 0.5 volume ('C', 3),
-
-        --     playNote 4.0 0.5 volume ('E', 3),
-        --     playNote 4.5 0.5 volume ('F', 3),
-        --     playNote 5.0 0.5 volume ('G', 3)
-        -- ]
     ]
 
 main :: IO ()
