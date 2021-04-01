382eb727e22b90c4e94aecfb933b7e0763e13315
Jort van Gorkum <jortvangorkum@gmail.com>
2021-04-01 10:41:11 +0200

    save merge changes

--- tests/test.hs
+++ tests/test.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Test.Tasty
 import Tests.Language
 import Tests.Notes
@@ -20,24 +19,3 @@ unitTestsTree = testGroup "UnitTests"
   , testGroup "Tests.Synthesizer.Structure" synthesizerStructureTests
   , testGroup "Tests.Synthesizer.Oscillator" oscillatorStructureTests
   , testGroup "Tests.Synthesizer.Modifiers.Envelopes" envelopesTests]
-=======
-import Test.Tasty
-import Tests.Language
-import Tests.Notes
-import Tests.Notes.Default
-import Tests.Synthesizer.Oscillator
-import Tests.Synthesizer.Structure
-
-main = defaultMain tests
-
-tests :: TestTree
-tests = testGroup "Tests" [unitTestsTree]
-
-unitTestsTree :: TestTree
-unitTestsTree = testGroup "UnitTests"
-  [ testGroup "Tests.Language" languageTests
-  , testGroup "Tests.Notes" notesTests
-  , testGroup "Tests.Notes.Default" defaultNotesTests
-  , testGroup "Tests.Synthesizer.Structure" synthesizerStructureTests
-  , testGroup "Tests.Synthesizer.Oscillator" oscillatorStructureTests ]
->>>>>>> Fixed exports for tests
