63792ae3a062462bc2a214ca4a4bd18d984587c2
Jort van Gorkum <jortvangorkum@gmail.com>
2021-04-01 10:44:49 +0200

    Added Length to import

--- src/Synthesizer/Modifiers/Envelopes.hs
+++ src/Synthesizer/Modifiers/Envelopes.hs
@@ -1,7 +1,8 @@
 module Synthesizer.Modifiers.Envelopes
   where
 
-import Synthesizer.Structure (Sample, SamplingRate, SoundEvent (SoundEvent))
+import Synthesizer.Structure (Length, Sample, SamplingRate,
+                              SoundEvent (SoundEvent))
 
 type Step          = Double
 type AttackLength  = Double
