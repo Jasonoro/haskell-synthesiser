8aea73f4185145e81ed27d50dc6f862632875b82
Vince van Noort <vincevannoort@gmail.com>
2021-04-08 10:44:19 +0200

    moonlight sonata

--- app/Main.hs
+++ app/Main.hs
@@ -18,24 +18,63 @@ import Synthesizer.Structure
 defaultVolume :: Amplitude
 defaultVolume = 25000
 
+volume = 0.5
+defaultDuration = 0.3
+longDuration = 1
+offset = 0.3
+pauze = 0.05
+
 musicPiece :: MusicPiece
 musicPiece = MusicPiece [
-    Instrument 440 defaultVolume (noteStrike 0.2 0.2 0.5 0.1) [
-      NoteEvent 0   0.5 1 (C ^= Four),
-      NoteEvent 0.5 0.5 1 (D ^= Four),
-      NoteEvent 1   0.5 1 (E ^= Four),
-      NoteEvent 1.5 0.5 1 (C ^= Four),
 
-      NoteEvent 2   0.5 1 (C ^= Four),
-      NoteEvent 2.5 0.5 1 (D ^= Four),
-      NoteEvent 3   0.5 1 (E ^= Four),
-      NoteEvent 3.5 0.5 1 (C ^= Four),
+    Instrument 440 defaultVolume (noteStrike 0.3 0.5 0.5 4) [
+      NoteEvent (noteOffset 0 0)   longDuration 0.2 (C ^= Three),
+      NoteEvent (noteOffset 0 0)   longDuration 0.2 (C ^= Two),
 
-      NoteEvent 4   0.5 1.3 (E ^= Four),
-      NoteEvent 4.5 0.5 1 (F ^= Four),
-      NoteEvent 5   0.5 1.3 (G ^= Four)
+      NoteEvent (noteOffset 12 4)  longDuration 0.2 (B ^= Three),
+      NoteEvent (noteOffset 12 4)  longDuration 0.2 (B ^= Two)
+    ],
+
+    Instrument 440 defaultVolume (noteStrike 0.05 0.1 1 0.05) [
+
+      -- first four
+      NoteEvent (noteOffset 0 0)   defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 1 0)   defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 2 0)   defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 3 1)   defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 4 1)   defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 5 1)   defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 6 2)   defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 7 2)   defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 8 2)   defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 9 3)   defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 10 3)  defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 11 3)  defaultDuration (volume * 0.7) (E ^= Four),
+
+      -- second four
+      NoteEvent (noteOffset 12 4)  defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 13 4)  defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 14 4)  defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 15 5)  defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 16 5)  defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 17 5)  defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 18 6)  defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 19 6)  defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 20 6)  defaultDuration (volume * 0.7) (E ^= Four),
+
+      NoteEvent (noteOffset 21 7)  defaultDuration volume         (G ^= Three),
+      NoteEvent (noteOffset 22 7)  defaultDuration (volume * 0.8) (C ^= Four),
+      NoteEvent (noteOffset 23 7)  defaultDuration (volume * 0.7) (E ^= Four)
+
     ]
   ]
+    where
+      noteOffset offsetAmount pauzeAmount = offset * offsetAmount + pauze * pauzeAmount
 
 
 mediumPrioritySignal :: SynSound
@@ -43,4 +82,4 @@ mediumPrioritySignal = convertMusicPieceToSynthesizer musicPiece
 
 main :: IO ()
 main = do
-    saveSignal "medium" mediumPrioritySignal
+    saveSignal "moonlight-sonata" mediumPrioritySignal
