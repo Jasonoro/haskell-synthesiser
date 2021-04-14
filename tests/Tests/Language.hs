6a6818809318f62081d4e08a22f1e810da52a3db
Vince van Noort <vincevannoort@gmail.com>
2021-04-14 23:38:43 +0200

    Added test for chord

--- tests/Tests/Language.hs
+++ tests/Tests/Language.hs
@@ -1,6 +1,7 @@
 module Tests.Language
   where
 
+import Language.Chords     (Chord (..), ChordType (..), getChordNotes)
 import Language.Modulators
 import Language.Notes
 import Language.Shifts
@@ -8,6 +9,7 @@ import Prelude             hiding ((^))
 import Test.Tasty.HUnit
 
 testNote = Note D Flat One
+testChord = Chord Major (Note C Flat One)
 testNotes = [testNote, testNote, testNote]
 
 languageTests =
@@ -37,6 +39,8 @@ languageTests =
     testCase "Modulate Tone - Combine"    $ testNote #= B #= A                  @?= Note A Flat One,
     testCase "Modulate Tone - Multiple"   $ testNotes #= A                      @?= [Note A Flat One, Note A Flat One, Note A Flat One],
     -- Combinding
-    testCase "Combining shift and modulations"
-    $ ((testNote #= B) ^= Zero) ^ 8                                             @?= Note B Flat Eight
+    testCase "Combining shift and modulations" $ ((testNote #= B) ^= Zero) ^ 8  @?= Note B Flat Eight,
+    -- Chords
+    testCase "Chord - Major"              $ getChordNotes testChord             @?= [Note C Flat One, Note E Flat One, Note G Flat One]
+
   ]
